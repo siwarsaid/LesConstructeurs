a440a08ae8aba9671cbae98c9e3720c3ec7ac708
siwar said <siwarsaid8@gmail.com>
2024-04-15 11:31:54 +0200

    quete les constructeurs

---
+++ LesConstructeurs/LesConstructeurs/Building.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesConstructeurs
+{
+    internal class Building
+    {
+      private int _nbetages;
+      private double _tailleBatiment;
+      private double _hauteurEtage=3;
+
+        public Building(int nbetages, double taille):this(nbetages)
+        {
+                    
+            _tailleBatiment = taille;
+            _hauteurEtage = GetFloorMaxSize();
+        }
+        public Building(int nbetages)
+        {
+            _nbetages = nbetages;
+            _tailleBatiment = Getsize();
+        }
+
+        public double GetFloorMaxSize()
+        {
+            return _tailleBatiment / _nbetages;
+        }
+        public int GetFloorCount()
+        {
+            return _nbetages;
+        }
+        public double Getsize()
+        {
+            return _hauteurEtage * _nbetages;
+        }
+    }
+}
---
+++ LesConstructeurs/LesConstructeurs/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LesConstructeurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building(8, 17.5);
+
+            Console.WriteLine("La taille du batiment : " + building.Getsize()+"\n");
+            Console.WriteLine("Le nombre detage du batiment : "+building.GetFloorCount()+"\n");
+            Console.WriteLine("La taille maximal de letage : " +building.GetFloorMaxSize()+"\n");
+           
+        }
+    }
+}
